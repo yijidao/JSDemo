b02881f15ab9625a61dee53b2d9e82315ebf4b71
wengzepeng <wengzepeng@pcitech.com>
2021-10-29 18:17:34 +0800

    demo

---
+++ antd-demo/src/component/ProjectPanel.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ProjectPanel extends Component {
+  static propTypes = {
+    prop: PropTypes
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    )
+  }
+}
