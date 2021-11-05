2287633577f9a1973f7170265cb358c5db5c3bf4
wengzepeng <wengzepeng@pcitech.com>
2021-11-05 14:48:33 +0800

    webpack less

--- antd-demo/src/component/ProjectPanel.js
+++ antd-demo/src/component/ProjectPanel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import './ProjectPanel.less';
 
 export default class ProjectPanel extends Component {
   // static propTypes = {
