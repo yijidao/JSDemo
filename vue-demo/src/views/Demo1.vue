3b8866e8358bcbe02a318ab56bdc41640a2cdf7c
wengzepeng <wengzepeng@pcitech.com>
2022-01-14 18:32:50 +0800

    vue demo

---
+++ vue-demo/src/views/Demo1.vue
@@ -0,0 +1,29 @@
+<template >
+    <div id="counter">Counter: {{ couter }}</div>
+    <div>
+        <p>{{message}}</p>
+        <input v-model="message" />
+        
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            couter: 0,
+            message: "1"
+        }
+    },
+    mounted() {
+        setInterval(() => {
+            this.couter++;
+        }, 1000);
+    },
+}
+
+
+
+</script>
+<style lang="css">
+</style>
